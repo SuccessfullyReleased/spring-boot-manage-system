9af042c449b54cf4acb289f55a84500c4256bd56
SuccessfullyReleased <1601435381@qq.com>
2019-05-24 14:05:41 +0800

    http协议修改

--- rbac/src/main/java/com/demo/rbac/RbacApplication.java
+++ rbac/src/main/java/com/demo/rbac/RbacApplication.java
@@ -3,6 +3,7 @@ package com.demo.rbac;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
